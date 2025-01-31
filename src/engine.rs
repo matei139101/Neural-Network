4752a235cab0ed4366a4399fe37a3c44efcbe490
matei139101 <matei139101@gmail.com>
2025-01-31 07:03:23 +0100

    Feat: Changed everything to use f32 instead of whatever else was used before for consistency

--- src/engine.rs
+++ src/engine.rs
@@ -1,18 +1,17 @@
 use crate::utils;
 
-pub fn run_engine(input_index_data: &[[i8; 5]], correction_data: &[bool], weights: &mut[[f32; 5]], generations: i32) {
+pub fn run_engine(input_index_data: &[[f32; 5]], correction_data: &[f32], weights: &mut[[f32; 5]], generations: u8) {
     let mut outputs: Vec<f32> = vec![];
     let mut corrected_outputs: Vec<f32> = vec![];
     let mut best_weights: [f32; 5] = [0f32; 5];
 
-    for generation in 0..generations {     
-        
-    for input_index in 0..input_index_data.len() {   
-        let output: [f32; 2] = network(input_index_data, weights, correction_data, input_index);
+    for generation in 0..generations {    
+        for input_index in 0..input_index_data.len() {   
+            let output: [f32; 2] = network(input_index_data, weights, correction_data, input_index);
 
-        outputs.push(output[0]);
-        corrected_outputs.push(output[1]);
-    }
+            outputs.push(output[0]);
+            corrected_outputs.push(output[1]);
+        }
 
         learn(outputs.clone(), weights, &mut best_weights);
 
@@ -20,7 +19,7 @@ pub fn run_engine(input_index_data: &[[i8; 5]], correction_data: &[bool], weight
     }
 }
 
-fn network(input_index_data: &[[i8; 5]], weights: &mut[[f32; 5]], correction_data: &[bool], input_index: usize) -> [f32; 2] {
+fn network(input_index_data: &[[f32; 5]], weights: &mut[[f32; 5]], correction_data: &[f32], input_index: usize) -> [f32; 2] {
         let mut output: f32 = utils::dot_product(&input_index_data[input_index], &weights[0]);
         output = utils::sygmoid(output);
 
